8fdc701493680fc693d9a6fe2ef0f0afd19347e0
N4sky1 <N4sky@yandex.ru>
2019-02-03 16:57:05 +0700

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,218 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var plumber = require("gulp-plumber");
+var autoprefixer = require("gulp-autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del");
+var posthtml = require("gulp-posthtml"); 
+var include = require("posthtml-include");
+var rename = require("gulp-rename"); 
+var svgstore = require("gulp-svgstore");
+var imagemin = require("gulp-imagemin");
+var	browserSync = require('browser-sync');
+var run = require("run-sequence");
+var paths = {
+  less:['project/less/style.less'],
+  html:['project/*.html']
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+//изз лесс в цсс и минимизирует цсс файл
+/////////////////////////////////////////////////////////////////////////////////
+/*gulp.task("style", function () { 
+	return gulp.src("project/less/main.less") 
+	.pipe(plumber()) 
+	.pipe(less())
+	.pipe(autoprefixer({
+			browsers: ['last 4 versions']
+		})) 
+	.pipe(gulp.dest("project/build/css/")) 
+	.pipe(minify())
+	.pipe(rename("main.min.css")) 
+	.pipe(gulp.dest("project/build/css/")); 
+});*/
+/*var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+
+gulp.task("style", function () { 
+	return gulp.src("project/scss/style.scss") 
+	.pipe(plumber()) 
+	.pipe(sass())
+	.pipe(autoprefixer({
+			browsers: ['last 4 versions']
+		})) 
+	.pipe(gulp.dest("project/build/css/")) 
+	.pipe(minify())
+	.pipe(rename("style.min.css")) 
+	.pipe(gulp.dest("project/build/css/")); 
+});*/
+var data = require('gulp-data');
+var stylus = require('gulp-stylus');
+ 
+ 
+// include, if you want to work with sourcemaps
+var sourcemaps = require('gulp-sourcemaps');
+ 
+// Get one .styl file and render
+gulp.task('style', function () {
+  return gulp.src('project/styl/style.styl')
+    .pipe(plumber())
+    .pipe(stylus())
+    .pipe(autoprefixer({
+			browsers: ['last 4 versions']
+		})) 
+    .pipe(gulp.dest('./css/build'))
+    .pipe(minify())
+	.pipe(rename("style.min.css"))
+	.pipe(gulp.dest("project/build/css/"));
+});
+//////////////////////////////////////////////////////////////////////////////////
+// сжимает изображения
+//////////////////////////////////////////////////////////////////////////////////
+gulp.task("compress-jpg", function () {
+	return gulp.src("project/img/**/*.jpg") 
+		.pipe(imagemin([
+			imagemin.jpegtran({progressive: true})
+		]))
+		.pipe(gulp.dest("project/img/"));
+	});
+//////////////////////////////////////////////////////////////////////////////////
+gulp.task("compress-png", function () {
+	return gulp.src("project/img/**/*.png") 
+		.pipe(imagemin([
+			imagemin.optipng({optimizationLevel: 3})
+		]))
+		.pipe(gulp.dest("project/img/"));
+	});
+//////////////////////////////////////////////////////////////////////////////////
+gulp.task("compress-svg", function () {
+	return gulp.src("project/img/**/*.svg") 
+		.pipe(imagemin([
+			imagemin.svgo() 
+		]))
+		.pipe(gulp.dest("project/img/"));
+	});
+/////////////////////////////////////////////////////////////////////////////////
+// svg спрайты
+/////////////////////////////////////////////////////////////////////////////////
+gulp.task("sprite", function () { 
+	return gulp.src("project/img/icon-*.svg")
+	.pipe(svgstore({ 
+		inlineSvg: true 
+	}))
+	.pipe(rename("sprite.svg"))
+	.pipe(gulp.dest("project/img/"));
+});
+
+				// webp не работает на вин7, нужен вин10
+				//var gulp = require("gulp"); 
+				//var webp = require("gulp-webp");
+
+				//gulp.task("webp", function () { 
+				//	return gulp.src("project/img/**/*.{png,jpg}") 
+				//	.pipe(webp({quality: 90}))
+				//	.pipe(gulp.dest("project/build/img/"));
+				//});
+
+/////////////////////////////////////////////////////////////////////////////////
+//пост html -вставляет в разметку вместо инклюдов свг или что там заинклюжено
+/////////////////////////////////////////////////////////////////////////////////
+/*gulp.task("html", function () { 
+	return gulp.src("project/*.html")
+	.pipe(posthtml([ 
+		include() 
+		]))
+	.pipe(gulp.dest("project/build/"));
+});*/
+
+var pug = require('gulp-pug');
+ 
+gulp.task('html', function buildHTML() {
+  return gulp.src('project/*.pug')
+  .pipe(pug({
+    // Your options in here.
+  }))
+  .pipe(gulp.dest("project/build/"));
+});
+/////////////////////////////////////////////////////////////////////////////////
+//копирование файлов из продакшн в билд
+/////////////////////////////////////////////////////////////////////////////////
+gulp.task("copy", function () { 
+	return gulp.src([ 
+	"project/fonts/**/*.{woff,woff2}", 	
+	"project/js/**/*.js",
+	"project/img/**/*.*"
+	], { 
+		base: "project"
+	}) 
+	.pipe(gulp.dest("project/build/")); 
+});
+/////////////////////////////////////////////////////////////////////////////////
+//отчистка папки build
+/////////////////////////////////////////////////////////////////////////////////
+ gulp.task("clean", function () { 
+	return del("project/build");
+});
+/////////////////////////////////////////////////////////////////////////////////
+//автопрефиксер
+/////////////////////////////////////////////////////////////////////////////////
+gulp.task('autoprefixer', function(){
+	gulp.src('project/css/main.css')
+	.pipe(autoprefixer({
+		browsers: ['last 4 versions']
+	}))
+	.pipe(gulp.dest('project/build/css/'));
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+// запускаем все функции для сборки билда
+/////////////////////////////////////////////////////////////////////////////////
+
+gulp.task("build", function (done) { 
+	run( 
+		"clean",
+		"copy", 
+		"style",
+		"compress-png",
+		"compress-jpg",
+		"compress-svg",
+		"sprite", 
+		"html",
+		"autoprefixer",
+		"reload",
+		done
+		 ); 
+});
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// сервер
+/////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'project/build/'
+    }
+  }) 
+});
+
+/////////////////////////////////////////////////////////////////////////////////
+// следильщик
+/////////////////////////////////////////////////////////////////////////////////
+  gulp.task('watcher',function(){
+    gulp.watch('project/styl/*.styl', ['build']);
+    gulp.watch('project/*.pug', ['build']);
+}); // работает с less и scss
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+// перезагрузка сервера, не обновляет страницу, что то нужно сделать с ним(если нужно)
+/////////////////////////////////////////////////////////////////////////////////
+ gulp.task('reload', function() {
+ 	browserSync.reload
+ });
